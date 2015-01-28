eb16560f59cc104a03d2e253dbe5b1f7bfe5fb6b
Nuzhdin Urii <nuzhdin.urii@gmail.com>
2015-01-28 12:56:21 +0400

    Epochize by event

--- R/epoch.by.event.R
+++ R/epoch.by.event.R
@@ -1,68 +1,94 @@
-signal <- matrix(0.0, nrow = 2^5, ncol = 62)
-pointer <- 0L
-si.times <- c()
-evs <- list()
-index <- integer64(1)
-
-epoch.by.event <- function(si = NULL, events = NULL)
-{
+cross.epochByEvent <- function(input, events){
+  signal <- matrix(0.0, nrow = 2^5, ncol = input$channels)
+  pointer <- 0L
+  si.times <- c()
+  evs <- list()
+  index <- integer64(1)
   
-  res <- NULL
-  timestamps <- NULL
-  #assign temporary signals and events
-  if(!is.null(si))
+  epoch.by.event <- function(si = NULL, events = NULL)
   {
-    if(nrow(si)+pointer >= nrow(signal))
+    
+    res <- NULL
+    timestamps <- NULL
+    #assign temporary signals and events
+    if(!is.null(si))
     {
-      tmp <- matrix(0.0, ncol=ncol(si), nrow=(nrow(signal)+nrow(si))*1.5)
-      Resonance:::rowsCopy(tmp,0, signal, 0, -1)
-      signal <<- tmp
-    }
-    
-    Resonance:::rowsCopy(signal, pointer, si, 0, -1)
-    pointer <<- pointer+nrow(si)
-    
-    si.times <<- append(si.times, attr(si, 'time'))
-  }
-  
-  if(!is.null(events))
-  {
-    evs <<- c(evs, events)
-  }
-  
-    
-  while( length(evs) && length(si.times) &&
-    evs[[1]]$time < si.times[[length(si.times)]]
-    )
-  {
-    current <- which(si.times >= evs[[1]]$time)[[1]]
-    
-    if(evs[[1]]$type)
-    {
-      if(index)
+      if(nrow(si)+pointer >= nrow(signal))
       {
-        res <- rbind(res, signal[index:current, ])
-        timestamps <- append(timestamps, si.times[index:current])
+        tmp <- matrix(0.0, ncol=ncol(si), nrow=(nrow(signal)+nrow(si))*1.5)
+        Resonance:::rowsCopy(tmp,0, signal, 0, -1)
+        signal <<- tmp
       }
-      index <<- current
+      
+      Resonance:::rowsCopy(signal, pointer, si, 0, -1)
+      pointer <<- pointer+nrow(si)
+      
+      si.times <<- append(si.times, attr(si, 'timestamps'))
     }
-    else
+    
+    if(!is.null(events))
     {
-      if(index)
-      {
-        res <- rbind(res, signal[index:current, ])
-        timestamps <- append(timestamps, si.times[index:current])
-        index <<- integer64(1)
-      }
+      evs <<- c(evs, events)
     }
-    evs <<- evs[-1]
+    
+      
+    while( length(evs) && length(si.times) &&
+      evs[[1]]$time < si.times[[length(si.times)]]
+      )
+    {
+      current <- which(si.times >= evs[[1]]$time)[[1]]
+      
+      if(evs[[1]]$type)
+      {
+        if(index)
+        {
+          res <- rbind(res, signal[index:current, ])
+          timestamps <- append(timestamps, si.times[index:current])
+        }
+        index <<- current
+      }
+      else
+      {
+        if(index)
+        {
+          res <- rbind(res, signal[index:current, ])
+          timestamps <- append(timestamps, si.times[index:current])
+          index <<- integer64(1)
+        }
+      }
+      evs <<- evs[-1]
+    }
+    
+    if(!is.null(res))
+    {
+      attr(res, 'timestamps') <- timestamps
+    }
+    
+    res
+    
   }
   
-  if(!is.null(res))
-  {
-    attr(res, 'time') <- timestamps
+  bp <- block.processor(input)
+  
+  bpcb <- function(d,e){
+    out <- epoch.by.event(d,e)
+    if(!is.null(out))
+    {
+      attr(out, 'timestamp') <- attr(out, 'timestamps')[[1]]
+      attr(out, 'timestamps') <- NULL
+      bp$emit(out)
+    }
   }
   
-  res
+  input$connect(function(db){
+    attr(db, 'timestamps') <- attr(db, 'timestamp')+ seq(from = 0, length.out = nrow(db), by = 1E9/input$samplingRate)
+    bpcb(db, NULL)
+  })
   
+  events$connect(function(db){
+    ev <- list(list(type=db, time=attr(db, 'timestamp')))
+    bpcb(NULL, ev)
+  })
+  
+  bp
 }
