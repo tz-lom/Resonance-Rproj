6dc45edddf4128f6834170f9097df9cdd2235457
Nuzhdin Urii <nuzhdin.urii@gmail.com>
2015-03-11 15:16:26 +0300

    Make references not crucial

--- R/signalProcessingHelpers.R
+++ R/signalProcessingHelpers.R
@@ -1,8 +1,9 @@
 #' Typical signal preprocessing for EEG
 #' 
-signalPreparation <- function(input, refs, low=0.1, high=40, notch=50){
-  pipeline(
-    pipe.references(input, refs),
-    pipe.bandFilter(, low, high, notch)
-  )
+signalPreparation <- function(input, refs=c(), low=0.1, high=40, notch=50){
+  out <- input
+  if(length(refs)>0){
+      out <- pipe.references(input, refs)
+  }
+  pipe.bandFilter(out, low, high, notch)  
 }
