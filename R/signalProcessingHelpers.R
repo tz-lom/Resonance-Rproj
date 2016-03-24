83343f6f6ee09a3bc6bff0bda85bdbea3620fa21
Nastya Fedorova <mayenok@gmail.com>
2016-03-24 13:10:25 +0300

    signal preparation fixed

--- R/signalProcessingHelpers.R
+++ R/signalProcessingHelpers.R
@@ -1,9 +1,17 @@
 #' Typical signal preprocessing for EEG
 #' 
-signalPreparation <- function(input, refs=c(), low=0.1, high=40, notch=50){
+signalPreparation <- function(input, channels=c(),refs=c(), low=0.1, high=40, notch=50){
   out <- input
-  if(length(refs)>0){
-      out <- pipe.references(input, refs)
+  if(length(channels)>0){
+    
+    M <- diag(nrow=SI(input)$channels)[channels, ,drop=F]
+    M[, refs] = -1/length(refs)
+    
+    out <- pipe.spatial(input, M)
+  } else {
+    if(length(refs)>0){
+        out <- pipe.references(input, refs)
+    }
   }
   pipe.bandFilter(out, low, high, notch)  
 }
