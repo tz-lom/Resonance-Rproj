ec07e76f917b9516c1fbd5d29dac4c0fc4500795
Nuzhdin Yury <nuzhin.urii@gmail.com>
2018-07-30 23:12:52 +0200

    Some tests are working

--- tests/testthat.R
+++ tests/testthat.R
@@ -2,15 +2,4 @@ library(testthat)
 library(Resonance)
 
 
-expect_streams_equal <- function(a, b, ..., info = NULL, label = NULL, expected.label = NULL){
-  lab_act <- make_label(object, label)
-  lab_exp <- make_label(expected, expected.label)
-
-  comp <- compare(object, expected, ...)
-  
-  expect(comp$equal, sprintf("%s not equal to %s.\n%s", lab_act, 
-                               lab_exp, comp$message), info = info)
-  invisible(a)
-}
-
 test_check("Resonance")
--- tests/testthat/test-cross.epochByEvent.R
+++ tests/testthat/test-cross.epochByEvent.R
@@ -1,23 +1,25 @@
 test_that("cross.epochByEvent", {
   Csi <- SI.channels(2, 100)
   Esi <- SI.event()
-  
+
   blocks <- list(
     DB.channels(Csi, 3, 1:2),
     DB.channels(Csi, 3+10*5E4, 26:75),
     DB.event(Esi, 30 , TRUE),
     DB.event(Esi, 3E4 , FALSE)
   )
+
+
+  code <- function(){
+    
+    process <- function(){
+      createOutput(cross.epochByEvent(input(1), input(2)), 'out')
+    }
   
-  
-  code <- "
-  process <- function(){
-  createOutput(cross.epochByEvent(input(1), input(2)), 'out')
   }
-  "
-  
+
   A <- run.online(list(Csi, Esi), blocks, code)
   B <- run.offline(list(Csi, Esi), blocks, code)
-  
+
   expect_equal(A, B)
 })
