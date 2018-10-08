867f59306b6505cb36802827c561b996f5b5a2d6
Nuzhdin Yury <nuzhin.urii@gmail.com>
2018-10-08 22:27:52 +0200

    More timer tests

--- tests/testthat/test-timers.R
+++ tests/testthat/test-timers.R
@@ -9,7 +9,16 @@ timer_test <- function(start, pass){
     online = function(input, pass, onTimeout=NULL){
       
       for(i in input){
-        startTimer(timeout = 500, singleShot = T)
+        args <- list(
+          timeout = 500,
+          singleShot = T,
+          data = TRUE,
+          call = 'onTimeout'
+        )
+        for(n in names(i)){
+          args[[n]] <- i[[n]]
+        }
+        do.call(startTimer, args)
       }
       
       if(is.null(onTimeout)){
@@ -33,17 +42,77 @@ code = "
 
 do_test <- function(blocks, reference){
   online <- run.online(list(si1, si2), blocks, code, env=environment())
-  expect_identical(online, list(out=reference))
+  expect_identical(list(out=reference), online)
 }
 
-test_that("Timed events happens", {
-  
+test_that("Timer triggers after last block", {
   do_test(
     list(
-      DB.event(si1, timeoption2ts(si1, seconds(1)), 'A')
+      DB.event(si1, timeoption2ts(si1, seconds(1)), list())
     ),
-    list(
-      DB.event(siO, timeoption2ts(siO, seconds(1.5)), 'A')
+    DBcombine(
+      DB.event(siO, timeoption2ts(siO, seconds(1.5)), TRUE)
     )
   )
 })
+
+test_that("Timer triggers after between blocks", {
+  do_test(
+    list(
+      DB.event(si1, timeoption2ts(si1, seconds(1)), list()),
+      DB.event(si2, timeoption2ts(si2, seconds(2)), 'B')
+    ),
+    DBcombine(
+      DB.event(siO, timeoption2ts(siO, seconds(1.5)), TRUE),
+      DB.event(siO, timeoption2ts(siO, seconds(2)), 'B')
+    )
+  )
+})
+
+test_that("Timers reorder correctly", {
+  do_test(
+    list(
+      DB.event(si1, timeoption2ts(si1, seconds(1)), list(data='a')),
+      DB.event(si1, timeoption2ts(si1, seconds(1.01)), list(data='b', timeout=400)),
+      DB.event(si2, timeoption2ts(si2, seconds(2)), 'Z')
+    ),
+    DBcombine(
+      DB.event(siO, timeoption2ts(siO, seconds(1.41)), 'b'),
+      DB.event(siO, timeoption2ts(siO, seconds(1.5)), 'a'),
+      DB.event(siO, timeoption2ts(siO, seconds(2)), 'Z')
+    )
+  )
+})
+
+test_that("Timers reorder correctly after last block", {
+  do_test(
+    list(
+      DB.event(si1, timeoption2ts(si1, seconds(1.01)), list(data='a')),
+      DB.event(si1, timeoption2ts(si1, seconds(1.02)), list(data='b', timeout=400)),
+      DB.event(si1, timeoption2ts(si1, seconds(1.03)), list(data='c', timeout=221))
+    ),
+    DBcombine(
+      DB.event(siO, timeoption2ts(siO, seconds(1.251)), 'c'),
+      DB.event(siO, timeoption2ts(siO, seconds(1.420)), 'b'),
+      DB.event(siO, timeoption2ts(siO, seconds(1.510)), 'a')
+    )
+  )
+})
+
+test_that("Timers reorder correctly separated by block", {
+  do_test(
+    list(
+      DB.event(si1, timeoption2ts(si1, seconds(1.01)), list(data='a')),
+      DB.event(si1, timeoption2ts(si1, seconds(1.02)), list(data='b', timeout=400)),
+      DB.event(si1, timeoption2ts(si1, seconds(1.03)), list(data='c', timeout=221)),
+      DB.event(si2, timeoption2ts(si2, seconds(1.31)), 'Z')
+    ),
+    DBcombine(
+      DB.event(siO, timeoption2ts(siO, seconds(1.251)), 'c'),
+      DB.event(siO, timeoption2ts(siO, seconds(1.310)), 'Z'),
+      DB.event(siO, timeoption2ts(siO, seconds(1.420)), 'b'),
+      DB.event(siO, timeoption2ts(siO, seconds(1.510)), 'a')
+    )
+  )
+})
+
